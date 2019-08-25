600a63b21e86bcb888b8b22f9846bd0e3e39bccb
SakshiShreya <shreyasakshi96@gmail.com>
2019-08-25 23:58:19 +0530

    Clear code to make boilerplate

--- src/app.js
+++ src/app.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import IndecisionApp from './components/IndecisionApp';
-
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my noilerplate</p>, document.getElementById('app'));
